d8689544afcedbf6061928fdd17fa6ef7c39bd48
Augusto Scarelli <augustoscarellis@gmail.com>
2019-11-21 14:18:34 -0300

    Teste1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e08c8e14250766ec8b2a0cc177371628.js"
+  "/precache-manifest.c12b10c235e06b4f456af13e784c7504.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
