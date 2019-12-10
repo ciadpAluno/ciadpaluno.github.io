e3bd9dd1bf3ae240b466e911ceb91abf669b3c90
Augusto Scarelli Silva <augustoscarellis@gmail.com>
2019-12-10 06:48:46 -0300

    New Ver. 01

---
+++ precache-manifest.382f2f61fa11bb82df7b7709427df852.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c58977c8608fdf08191d09db233f4390",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db1506ae5806845c7a3c",
+    "url": "/static/css/2.c829125e.chunk.css"
+  },
+  {
+    "revision": "2ab8e01db39096a997e5",
+    "url": "/static/css/main.0ef5aee3.chunk.css"
+  },
+  {
+    "revision": "db1506ae5806845c7a3c",
+    "url": "/static/js/2.67389852.chunk.js"
+  },
+  {
+    "revision": "2ab8e01db39096a997e5",
+    "url": "/static/js/main.d7a145fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9d72d0285b658aef58a63ec9b00c5904",
+    "url": "/static/media/ic_ripple_loading.9d72d028.svg"
+  }
+]);
